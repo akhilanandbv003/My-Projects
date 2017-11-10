1f841add10de1865415cae7de91334bd88a32359
akhilanand benkal venkanna <akhilanand.venkanna@monsanto.com>
2017-11-10 04:37:32 -0600

    Updated console producer script

--- spark2.2/src/main/scala/wip/KafkaSparkProducer.scala
+++ spark2.2/src/main/scala/wip/KafkaSparkProducer.scala
@@ -2,11 +2,14 @@ package wip
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.OutputMode
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql._
-import org.apache.spark.sql._
+import org.apache.spark.sql.types.{StringType, StructField, StructType, _}
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.SparkSession
 
+
+//  ~/Downloads/confluent-3.3.0/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic topic1 --from-beginning
+//./kafka-topics --list --zookeeper localhost:2181
+//./kafka-topics --delete --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
 
 object KafkaSparkProducer {
   def main(args: Array[String]): Unit = {
@@ -15,42 +18,37 @@ object KafkaSparkProducer {
       .master("local")
       .appName("Consumer")
       .getOrCreate()
-
-    val schema = StructType(Array(
-      StructField("Date", StringType),
-      StructField("Open", StringType),
-      StructField("High", StringType),
-      StructField("Low", StringType),
-      StructField("Close", StringType),
-      StructField("Volume", StringType),
-      StructField("AdjClose", StringType)
-    ))
-
     import sparkSession.implicits._
+
+    val schema = StructType(
+      StructField("id", StringType) ::
+        StructField("name", StringType) :: Nil)
+
+
+    //case class Apple(Date: String, Open: String, High: String, Low: String, Close: String, Volume: String, AdjClose: String)
+
+
 
     //create stream from folder
     val fileStreamDf = sparkSession.readStream
       .option("header", "true")
       .schema(schema)
-      .csv("/Users/avenk3/spark/CCA175/spark2/src/main/resources/csv")
+      .csv("/Users/avenk3/spark/My-Projects/spark2.2/src/main/resources/csv")
 
-
-
-   /* Consolw Sink
-   val query = fileStreamDf.writeStream
+    /* //Console Sink
+    val query = ds.writeStream
       .format("console")
       .outputMode(OutputMode.Append()).start()
     query.awaitTermination()
-    */
-  //val ds = fileStreamDf.selectExpr( "Volume as value").as[String]
+   */
+//val ds=fileStreamDf.selectExpr("CAST(id AS STRING)", "CAST(name AS STRING) as value")
 
-    //val ds = fileStreamDf.select(struct("Description", "InvoiceNo").alias("complex")) "Date as key "
-    fileStreamDf.withColumn("value","struct(fileStreamDf.columns.head, fileStreamDf.columns.tail: _*)").show(false)
 
+    //Kafka sink
     val q= fileStreamDf.writeStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("topic", "topic1")
+      .option("topic", "akhil")
      .option("checkpointLocation","/tmp/")
       .start()
 
